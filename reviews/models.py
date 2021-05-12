229f3a5f72df5a983be9706c5b135345b54b100c
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-12 13:25:02 +0900

    Refactor:멘토 리뷰 반영(reviews.models.Review review_image에 FK로 걸어놓은 클래스명 수정(Review_Image-> ReviewImage)

--- reviews/models.py
+++ reviews/models.py
@@ -10,7 +10,7 @@ class ReviewImage(models.Model):
 class Review(models.Model):
     product          = models.ForeignKey('products.Product',on_delete=models.CASCADE)
     user             = models.ForeignKey('users.User',on_delete=models.CASCADE)   
-    review_image     = models.ForeignKey('Review_Image',on_delete=models.CASCADE)
+    review_image     = models.ForeignKey('ReviewImage',on_delete=models.CASCADE)
     rating           = models.IntegerField()
     text             = models.CharField(max_length=2000)
     created_at       = models.DateTimeField(auto_now_add=True)
