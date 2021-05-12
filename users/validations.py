fa8cc2b9a773bdb766f3d667bc0f2e2849234adf
111geon <yangdaz3@gmail.com>
2021-05-12 15:21:24 +0900

    ADD: 회원가입, 로그인 기능 작성 완료

--- users/validations.py
+++ users/validations.py
@@ -3,24 +3,40 @@ import re
 from users.models import User
 
 class Validation:
-    def validate_email(self, mail):
-        regex = re.compile('[a-zA-Z0-9\.\_\+\-]+\@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\.]+')
-        if not regex.match(mail):
+    def validate_email(self, email):
+        regex = re.compile('^[0-9a-z\-\_]{5,20}$')
+        if not regex.match(email):
             return False
         return True
 
     def validate_password(self, password):
-        regex = re.compile('[A-Za-z0-9]{8,}')
+        regex = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$')
         if not regex.match(password):
             return False
         return True
 
-    def validate_duplication(self, email, phone, nickname):
+    def validate_name(self, name):
+        regex = re.compile('^[가-힣a-zA-Z]+$')
+        if not regex.match(name):
+            return False
+        return True
+
+    def validate_birth_date(self, birth_date):
+        regex = re.compile('^[\d]{4}-[\d]{2}-[\d]{2}$')
+        if not regex.match(birth_date):
+            return False
+        return True
+
+    def validate_phone_number(self, phone_number):
+        regex = re.compile('^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+        if not regex.match(phone_number):
+            return False
+        return True
+
+    def validate_duplication(self, email, phone_number):
         if User.objects.filter(email=email):
             return False
-        elif phone and User.objects.filter(phone=phone):
-            return False
-        elif nickname and User.objects.filter(nickname=nickname):
+        elif User.objects.filter(phone_number=phone_number):
             return False
         return True
 
--- users/views.py
+++ users/views.py
@@ -13,37 +13,50 @@ from my_settings            import SECRET_KEY
 class SignupView(View):
     def post(self, request):
         try:
-            data         = json.loads(request.body)
-            email        = data['email']
-            password     = data['password']
-            name         = data['name']
-            birth_date   = data['birth_date']
-            phone_number = data['phone_number']
-            points       = 0
-            gender       = data['gender']
-#            gender       = 
+            data          = json.loads(request.body)
+            email         = data['email']
+            password      = data['password']
+            name          = data['name']
+            birth_date    = data['birth_date']
+            phone_number  = data['phone_number']
+            gender_string = data['gender']
+            gender        = Gender.objects.get(gender=gender_string)
 
-#            if not Validation.validate_email(self, email):
-#                return JsonResponse({'message': 'Error email'}, status=400)
-
-#            if not Validation.validate_password(self, password):
-#                return JsonResponse({'message': 'Error password'}, status=400)
-
-#            if not Validation.validate_duplication(self, email, phone, nickname):
-#                return JsonResponse({'message': 'Already exist'}, status=400)
+            if not Validation.validate_email(self, email):
+                return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
+            if not Validation.validate_password(self, password):
+                return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=400)
+            if not Validation.validate_name(self, name):
+                return JsonResponse({'MESSAGE':'INVALID_NAME'}, status=400)
+            if not Validation.validate_birth_date(self, birth_date):
+                return JsonResponse({'MESSAGE':'INVALID_BIRTH_DATE'}, status=400)
+            if not Validation.validate_phone_number(self, phone_number):
+                return JsonResponse({'MESSAGE':'INVALID_PHONE_NUMBER'}, status=400)
+            if not Validation.validate_duplication(self, email, phone_number):
+                return JsonResponse({'MESSAGE':'DUPLICATED_USER'}, status=400)
 
 
             password = bcrypt.hashpw(
                     password.encode('utf-8'),
                     bcrypt.gensalt()
             ).decode('utf-8')
-            User.objects.create(email=email, password=password, phone=phone, nickname=nickname)
+
+            User.objects.create(
+                    email        = email,
+                    password     = password,
+                    name         = name,
+                    birth_date   = birth_date,
+                    phone_number = phone_number,
+                    gender       = gender
+            )
 
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
         except JSONDecodeError:
             return JsonResponse({'MESSAGE': 'NO_BODY'}, status=400)
+        except Gender.DoesNotExist:
+            return JsonResponse({'MESSAGE': 'INVALID_GENDER'}, status=400)
 
 
 class LoginView(View):
@@ -54,11 +67,11 @@ class LoginView(View):
             password = data['password']
 
             users = User.objects.filter(email=email)
-            if not users:
+            if not users.exists():
                 return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=401)
 
-            user = users[0]
-            if password != user.password:
+            user = users.first()
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
                 return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=401)
             
             access_token = jwt.encode(
