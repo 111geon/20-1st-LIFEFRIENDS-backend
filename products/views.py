b4267eef1208a62c3011c55f7c1360347a00b511
111geon <yangdaz3@gmail.com>
2021-05-18 01:40:01 +0900

    MODIFY: 기존 path parameter로 처리하던 방식을 query parameter로 처리하는 방식으로 수정 및 코드 리팩토링

--- products/views.py
+++ products/views.py
@@ -29,93 +29,75 @@ class CategoryView(View):
             results    = self.makeResults(categories)
         total_num = len(results)
 
-        sort = request.GET.get('sort', None)
-        if not sort or sort == "POPULAR":
-            results = sorted(
-                    results, 
-                    key= lambda product: product['clicks'], 
-                    reverse = True
-                    )
-        elif sort == "TOTALSALE":
-            results = sorted(
-                    results, 
-                    key= lambda product: product['sold'], 
-                    reverse = True
-                    )
-        elif sort == "LOWPRICE":
-            results = sorted(
-                    results,
-                    key= lambda product: product['cost']
-                    )
-        elif sort == "RECENT":
-            results = sorted(
-                    results,
-                    key= lambda product: product['created_at'],
-                    reverse = True
-                    )
-        elif sort == "REVIEW":
-            results = sorted(
-                    results,
-                    key= lambda product: product['reviewCount'],
-                    reverse = True
-                    )
-        elif sort == "SATISFACTION":
-            results = sorted(
-                    results,
-                    key= lambda product: product['rating'],
-                    reverse = True
-                    )
-
-        size = request.GET.get('size', '40')
+        sort     = request.GET.get('sort', None)
+        sort_std = {
+                None           : ['clicks', True],
+                'POPULAR'      : ['clicks', True],
+                'TOTALSALE'    : ['sold', True],
+                'LOWPRICE'     : ['cost', False],
+                'RECENT'       : ['created_at', True],
+                'REVIEW'       : ['reviewCount', True],
+                'SATISFACTION' : ['rating', True]
+                }
+        results = sorted(
+                results,
+                key = lambda product: product[sort_std[sort][0]],
+                reverse = sort_std[sort][1]
+                )
+                
+        size = request.GET.get('size', '10')
         size = int(size)
-
         page = request.GET.get('page', '1')
         page = int(page)
 
         results = results[(page-1)*size:page*size]
-
         return JsonResponse({'MESSAGE': results, 'TOTAL_NUM': total_num}, status=200)
 
 
     def makeResults(self, categories):
         results = []
+        
+        products = []
         for category in categories:
-            if isinstance(category, Theme):
-                products = Product.objects.filter(theme=category)
-            else:
-                products = Product.objects.filter(category=category)
-            for product in products:
-                images     = product.productimage_set.all()
-                image_urls = [image.url[1:-1] if image.url[0]!='h' else image.url for image in images]
+            try:
+                products += list(Product.objects.filter(theme=category))
+            except ValueError:
+                products += list(Product.objects.filter(category=category))
 
-                product_sizes   = ProductSize.objects.filter(product=product)
-                product_orders  = ProductOrder.objects.none()
-                product_reviews = Review.objects.none()
-                for product_size in product_sizes:
-                    product_orders |= ProductOrder.objects.filter(
-                            Q(product_size=product_size) & 
-                            ~Q(status=Status.objects.get(id=1))
-                            ).exclude(status=Status.objects.get(id=5)
-                            )
-                    product_reviews |= product_size.review_set.all()
-                sold = product_orders.aggregate(Sum('quantity'))['quantity__sum'] if product_orders.exists() else 0
-                review_count = product_reviews.count() if product_reviews.exists() else 0
-                rating       = product_reviews.aggregate(Avg('rating'))['rating__avg'] if product_reviews.exists() else 0
+        for product in products:
+            images     = product.productimage_set.all()
+            image_urls = [image.url[1:-1] if image.url[0]!='h' else image.url for image in images]
 
-                results.append(
-                    {
-                        'id'          : product.id,
-                        'name'        : product.name,
-                        'cost'        : int(product.cost),
-                        'created_at'  : product.created_at,
-                        'clicks'      : product.clicks,
-                        'imgUrl'      : image_urls[0],
-                        'imgAlt'      : product.name,
-                        'reviewCount' : review_count,
-                        'rating'      : rating,
-                        'sold'        : sold,
-                    }
-                )
+            sold            = 0
+            review_count    = 0
+            rating          = 0
+            product_sizes   = product.productsize_set.all()
+            for product_size in product_sizes:
+                product_orders = product_size.productorder_set.filter(
+                        ~Q(status_id=1) &
+                        ~Q(status_id=5)
+                        )
+                sold += product_orders.aggregate(Sum('quantity'))['quantity__sum'] if product_orders.exists() else 0
+
+                product_reviews  = product_size.review_set.all()
+                review_count    += len(product_reviews)
+                rating          += product_reviews.aggregate(Sum('rating'))['rating__sum'] if product_reviews.exists() else 0
+            rating = rating / review_count if review_count else 0
+
+            results.append(
+                {
+                    'id'          : product.id,
+                    'name'        : product.name,
+                    'cost'        : int(product.cost),
+                    'created_at'  : product.created_at,
+                    'clicks'      : product.clicks,
+                    'imgUrl'      : image_urls[0],
+                    'imgAlt'      : product.name,
+                    'reviewCount' : review_count,
+                    'rating'      : rating,
+                    'sold'        : sold,
+                }
+            )
         return results
 
 
