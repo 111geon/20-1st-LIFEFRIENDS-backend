2acf8640a448572d261f7f88d143916b5d0e331f
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-18 22:59:16 +0900

    Modify: try, except 문 추가

--- products/views.py
+++ products/views.py
@@ -6,20 +6,21 @@ class MenuNameView(View):
     def get(self,request):
         menus    = Menu.objects.all()
         menulist = []
-        
-        Review.objects.productsize__productorder__status
+        try:
+            for menu in menus:
+                themes    = menu.theme_set.all() 
+                themelist = [{'categoryid' : theme.id, 'categoryname' : theme.name} for theme in themes if theme.name != menu.name]
+                
+                categories   = menu.category_set.all()
+                categorylist = [{'categoryid': category.id, 'categoryname ' : category.name} for category in categories]
+                
+                menulist.append({
+                    'menuid'           : menu.id,
+                    'menuname'         : menu.name,
+                    'categorylist'     : categorylist+themelist
+                })
+                
+            return JsonResponse({'results':menulist}, status=200)
 
-        for menu in menus:
-            themes    = menu.theme_set.all() 
-            themelist = [{'categoryid' : theme.id, 'categoryname' : theme.name} for theme in themes if theme.name != menu.name]
-            
-            categories   = menu.category_set.all()
-            categorylist = [{'categoryid': category.id, 'categoryname ' : category.name} for category in categories]
-            
-            menulist.append({
-                'menuid'           : menu.id,
-                'menuname'         : menu.name,
-                'categorylist'     : categorylist+themelist
-            })
-            
-        return JsonResponse({'results':menulist}, status=200)
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
