25b37c845ce79496e576b8975591d43176298410
111geon <yangdaz3@gmail.com>
2021-05-18 19:25:10 +0900

    ADD: 로그인 시 발급하는 JWT에 iat 추가, 및 로그인 확인하는 데코레이터 작성

--- users/validations.py
+++ users/validations.py
@@ -1,8 +1,39 @@
 import re
+import jwt
+import datetime
 
+from django.http  import JsonResponse
+
+from my_settings  import SECRET_KEY
 from users.models import User
 
 class Validation:
+    def validate_login(func):
+        def wrapper(self, request):
+            try:
+                access_token = request.headers.get("AUTHORIZATION", None)
+                if not access_token:
+                    return JsonResponse({'MESSAGE': 'LOGIN_REQUIRED'}, status=401)
+
+                token_payload = jwt.decode(
+                        access_token,
+                        SECRET_KEY,
+                        algorithms="HS256"
+                        )
+
+                expiration_delta = 600
+                now = datetime.datetime.now().timestamp()
+                if now > token_payload['iat'] + expiration_delta:
+                    return JsonResponse({'MESSAGE': 'TOKEN_EXPIRED'}, status=401)
+
+                request.account = User.objects.get(id=token_payload['user_id'])
+                return func(self, request)
+            except jwt.DecodeError:
+                return JsonResponse({'MESSAGE': 'INVALID_JWT'}, status=401)
+            except User.DoesNotExist:
+                return JsonResponse({'MESSAGE': 'INVALID_USER'}, status=401)
+        return wrapper
+
     def validate_email(self, email):
         regex = re.compile('^[0-9a-z\-\_]{5,20}$')
         if not regex.match(email):
