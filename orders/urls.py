0d467a9134ed3a886c14795cd1cd0d7484f3209a
111geon <yangdaz3@gmail.com>
2021-05-18 15:05:26 +0900

    in progress

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-#from orders.views import OrderView
+from orders.views import CartView
 
 urlpatterns = [
-#    path('', OrderView.as_view()),
+    path('/cart', CartView.as_view()),
 ]
 
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http       import JsonResponse
+from django.views      import View
+
+from orders.models     import ProductOrder
+from users.validations import Validation
+
+class CartView(View):
+    @Validation.validate_login
+    def post(self, request):
+        
+
+        return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
+
