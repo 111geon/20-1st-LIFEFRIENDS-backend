227d17d975ece0623307d87a2de53c9f4d20127e
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-18 21:49:15 +0900

    Modify: Review 수정 및 list comprehension 사용"

--- products/views.py
+++ products/views.py
@@ -2,42 +2,39 @@ import json
 import random
 from typing import Text
 
-from django.http     import JsonResponse
-from django.views    import View
+from django.http      import JsonResponse
+from django.views     import View
 from django.db.models import Count
 
 from products.models import Product
-from reviews.models  import Review
 
 class SearchView(View):
     def get(self,request):
-        product_name = request.GET.get('search', None)
+        product_name = request.GET.get('search')
         products = Product.objects.filter(name__contains=product_name)
-        
-        sort = request.GET.get('sort', None)
-        if sort == None:
-            sorted_products = products.order_by('name')
+        try: 
+            sort = request.GET.get('sort', None)
+            if sort is None:
+                sorted_products = products.order_by('name')
+            if sort == 'LOWPRICE':
+                sorted_products = products.order_by('cost')
+            if sort == 'HIGHPRICE':
+                sorted_products = products.order_by('-cost')
+            if sort == 'RECENT':
+                sorted_products = products.order_by('created_at')
+            if sort == 'REVIEW':
+                a =Product.objects.filter(name__contains='쿠션')
+                sorted_products = a.annotate(count_review=Count('productsize__review')).order_by('count_review')
 
-        if sort == 'LOWPRICE':
-            sorted_products = products.order_by('cost')
-        if sort == 'HIGHPRICE':
-            sorted_products = products.order_by('-cost')
-        if sort == 'RECENT':
-            sorted_products = products.order_by('created_at')
-        if sort == 'REVIEW':
-            sorted_products = products.annoate(count_review=Count('review').order_by('count_review')
-
-        results = []
-        for product in sorted_products:
-            product_info = {
+            product_info = [{
                 'category'  : product.category.name,
                 'name'      : product.name,
                 'cost'      : product.cost,
                 'image_url' : product.productimage_set.first().url,
                 'created_at': product.created_at
-            }
-            results.append(product_info)
+            } for product in sorted_products]
             
-        count_products = len(results)
-
-        return JsonResponse({'results':results, 'count_products': count_products}, status=200)
+            return JsonResponse({'results':product_info}, status=200)
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KeyError'}, status=200)
