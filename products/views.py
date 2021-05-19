a63d7b74cdb9999e843b5377e1328e8adc9eeec2
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-19 20:18:47 +0900

    Modify: 리턴 메세지 수정

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ class ProductView(View):
     def get(self,request):
         product_id = request.GET.get('id', None)
         if not product_id:     
-            return JsonResponse({'MESSAGE':'NOT_FOUND_ID'}, status=400)   
+            return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT_ID'}, status=400)   
         if int(product_id) > Product.objects.count():
             return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT'}, status=400)   
         
