177b2a7a411c85dfdca2158c36eca838d56c2d61
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-19 18:27:41 +0900

    Add:구매 뷰 추가 진행중"

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from products.views import ProductView
 
 urlpatterns = [
     path('', ProductView.as_view()),
+    path('/purchase/<int:product_id>', ProductView.as_view()),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 import json
+from json import decoder
 import random
 from typing import Text
 
@@ -6,8 +7,25 @@ from django.http     import JsonResponse
 from django.views    import View
 
 from products.models import Product
+from users.validations import Validation
 
 class ProductView(View):
+#     @Validation.validate_login
+#     def post(self,request):
+#         user = request.account
+#         data = json.load(request.body)
+        
+#         if data['product_name'] not in product.name:
+#             return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT'}, status=400)         
+        
+#         product_size = product.size_set.get(name=data['product_size']
+#         quantity = data['quantity']
+
+# # 수량
+# # 가격
+# # 장바구니 담긴개수
+# # 몇개가 담겼습니다 ~ 
+
     def get(self,request):
         product_id = request.GET.get('id', None)
         if not product_id:     
@@ -27,3 +45,4 @@ class ProductView(View):
             'size'             : [product_size.name for product_size in product.size_set.all()]
         }
         return JsonResponse({'productdetail':productdetail}, status=200)
+
