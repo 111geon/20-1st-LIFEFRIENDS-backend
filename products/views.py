9952b0ee7937ba291cfbeefd42393dce419d087c
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-19 19:19:50 +0900

    Modify: purchase 뷰 수정진행

--- products/views.py
+++ products/views.py
@@ -7,25 +7,39 @@ from django.http     import JsonResponse
 from django.views    import View
 
 from products.models import Product
-from users.validations import Validation
+# from users.validations import Validation
 
 class ProductView(View):
-#     @Validation.validate_login
-#     def post(self,request):
-#         user = request.account
-#         data = json.load(request.body)
+    # @Validation.validate_login
+    def post(self,request,product_id):
+        # user = request.account
+        data    = json.loads(request.body)
         
-#         if data['product_name'] not in product.name:
-#             return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT'}, status=400)         
+        try: 
+            product  = Product.objects.filter(id=product_id) 
+            size     = product.size_set.get(name=data['product_size'])
+            quantity = data['quantity']
+            
+            is_product      = True if product.exist() else False
+            is_product_size = True if size.exist() else False
+
+            if not is_product:    
+                return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=400)  
+            if not is_product_size:    
+                return JsonResponse({'MESSAGE':'INVALID_SIZE'}, status=400)   
+
+            results = {
+                'product_size' : size.name,
+                'product_quantity' : quantity,
+                'product_in_cart'  : quantity,
+                # 'user'
+            }     
+
+            return JsonResponse({'RESULTS': results}, status=200)
         
-#         product_size = product.size_set.get(name=data['product_size']
-#         quantity = data['quantity']
-
-# # 수량
-# # 가격
-# # 장바구니 담긴개수
-# # 몇개가 담겼습니다 ~ 
-
+        except KeyError:
+                return JsonResponse({'MESSAGE':'NEED_INPUT'}, status=400)  
+            
     def get(self,request):
         product_id = request.GET.get('id', None)
         if not product_id:     
