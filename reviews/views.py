98eb2e4b12bf9f0b5b3fe4765ca52ea7585142c6
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-20 16:59:59 +0900

    Modify: 리뷰 등록 및 리뷰 겟 수정"

--- reviews/views.py
+++ reviews/views.py
@@ -4,13 +4,15 @@ from json import JSONDecodeError
 
 from django.http import JsonResponse
 from django.views import View
-from django.db.models import F, Sum, Count, Case, When, Avg
+from django.db.models import Count, Avg
 
 
 from reviews.models import ReviewImage
-from products.models import Product
+from products.models import Product, Size
 from orders.models  import ProductOrder
 from reviews.models import Review, ReviewImage
+from users.models import User
+
 from users.validations import Validation
 
 class ReviewView(View):
@@ -18,8 +20,11 @@ class ReviewView(View):
     def post(self,request,product_id):
         try:
             data = json.loads(request.body)
-            product  = Product.objects.get(id=product_id) 
-            size     = product.size_set.get(name=data['product_size'])   
+            
+            product     = Product.objects.get(id=product_id) 
+            size        = Size.objects.get(name=data['product_size'])
+            productsize = product.productsize_set.get(product_id=product_id, size_id=size)
+            user        = User.objects.get(id=request.account.id)
 
             if not Product.objects.filter(id=product_id).exists():    
                 return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=400)  
@@ -31,8 +36,8 @@ class ReviewView(View):
                 return JsonResponse({'MESSAGE':'NO_PURCHASE_HISTORY'}, status=400)  
 
             review_info = Review.objects.create(
-                product_size = size.name,
-                user_name    = request.account.name,
+                product_size = productsize,
+                user         = user,
                 rating       = data['rating'],
                 text         = data['text'],
             )
@@ -42,7 +47,7 @@ class ReviewView(View):
                 review_id        = review_info.id,
             )
 
-            return JsonResponse({'REVIEW':review_info, 'REVIEW_IMAGE': review_image}, status=400)
+            return JsonResponse({'REVIEW': 'SUCCESS'}, status=200)
 
         except KeyError: 
             return JsonResponse({'MESSAGE':'INVALID_INPUT'}, status=400)
@@ -55,20 +60,25 @@ class ReviewView(View):
             if not Product.objects.filter(id=product_id).exists():    
                 return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=400)  
             
-            reviews = [productsize.review_set.all() for productsize in product.productsize_set.all()]
-            review_info = [{
-                'user_name'    : review,
-                'created_at'   : review.created_at,
-                'product_size' : review.product_size.size.name,
-                'text'         : review.text,
-                'review_image' : [review_images.review_image_url for review_images in review.reiewimage_set.all()],
-                'rating'       : review,
-                'rate_average' : reviews.aggregate(rate_average=Avg('rating')),
-                'rate_count'   : reviews.annotate(rate_count=Count('rating')),
-                'total_review_count' : reviews.aggregate(total_review_count=Sum('review')),
-                'photo_review_count' : reviews.aggregate(photo_review_count=Count('reviewimage')),
-                } for review in reviews]
+            for productsize in product.productsize_set.all():
+                reviews = productsize.review_set.all()
+                review_info = [{
+                    'user_name'    : review.user.name,
+                    'created_at'   : review.created_at,
+                    'product_size' : review.product_size.size.name,
+                    'text'         : review.text,
+                    'review_image' : [review_images.review_image_url for review_images in review.reviewimage_set.all()],
+                    'rating'       : review.rating,
+                    } for review in productsize.review_set.all()]
+           
+                review_summary = {
+                    'rate_average' : reviews.aggregate(Avg('rating')),
+                    'rate_count'   : [reviews_count for reviews_count in reviews.values('rating').annotate(rate_count=Count('rating'))],
+                    'total_review_count' : reviews.aggregate(Count('id')),
+                    'photo_review_count' : reviews.aggregate(Count('reviewimage'))
+                }
 
-            return JsonResponse({'review_info' : review_info}, status=200)
+                return JsonResponse({'review_info' :review_info, 'review_summary': review_summary}, status=200)
+        
         except KeyError:
             return JsonResponse({'MESSAGE':'INVALID_PRODUCT'}, status=400)  
