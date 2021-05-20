d0c7a4b235f45577080c020fa9f4feeb0c7002da
seungho shin <shinisgood@seunghoui-MacBookAir.local>
2021-05-20 20:53:37 +0900

    Refactor: exception처리, path parameter 으로 수정"

--- products/views.py
+++ products/views.py
@@ -8,31 +8,38 @@ from django.http                import JsonResponse
 from django.views               import View
 from django.db.models           import Q, Count, Sum, Avg
 from django.db.models.functions import Coalesce
-from users.validations import Validation
+from django.core.exceptions     import ObjectDoesNotExist
 
-from products.models  import Menu, Product
+from users.validations          import Validation
+from products.models            import Menu, Product
 
 class ProductView(View):
-    def get(self,request):
-        product_id = request.GET.get('id', None)
-        if not product_id:     
-            return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT_ID'}, status=400)   
-        if int(product_id) > Product.objects.count():
-            return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT'}, status=400)   
-        
-        product    = Product.objects.get(id=product_id)
-        productdetail = {
-            'product_id'       : product.id,
-            'images'           : [product_images.url for product_images in product.productimage_set.all()],
-            'menu'             : product.category.menu.name,
-            'category'         : product.category.name,
-            'name'             : product.name,
-            'cost'             : product.cost,
-            'clicks'           : product.clicks,
-            'description'      : product.description_iamge_url,
-            'size'             : [product_size.name for product_size in product.size_set.all()]
-        }
-        return JsonResponse({'productdetail':productdetail}, status=200)
+    def get(self,request, product_id):
+        try: 
+            product  = Product.objects.get(id=product_id) 
+            if not Product.objects.filter(id=product_id).exists():    
+                return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT_ID'}, status=400)   
+            if int(product_id) > Product.objects.count():
+                return JsonResponse({'MESSAGE':'NOT_FOUND_PRODUCT'}, status=400)   
+            
+            product    = Product.objects.get(id=product_id)
+            productdetail = {
+                'product_id'       : product.id,
+                'images'           : [product_images.url for product_images in product.productimage_set.all()],
+                'menu'             : product.category.menu.name,
+                'category'         : product.category.name,
+                'name'             : product.name,
+                'cost'             : product.cost,
+                'clicks'           : product.clicks,
+                'description'      : product.description_iamge_url,
+                'size'             : [product_size.name for product_size in product.size_set.all()]
+            }
+            return JsonResponse({'productdetail':productdetail}, status=200)
+       
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KeyError'}, status=200)
+        except ObjectDoesNotExist:
+            return JsonResponse({'MESSAGE':'NO_PRODUCT'}, status=200)
 
 class ProductListView(View):
     def get(self, request):
@@ -131,6 +138,7 @@ class SearchView(View):
         
         except KeyError:
             return JsonResponse({'MESSAGE':'KeyError'}, status=200)
+
 class MenuView(View):
     def get(self,request):
         try:
